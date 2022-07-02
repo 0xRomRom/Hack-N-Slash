c60d07eb27d2c8a4ece7a389c546ae0379d1a98a
0xromrom <roman0811@live.nl>
2022-07-02 15:57:47 -0700

    Moving character

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ const player = new Fighter({
     x: 0,
     y: 0,
   },
+  imageSrc: "./img/Sprites/idle.png",
+  framesMax: 8,
+  scale: 2.5,
+  offset: {
+    x: 115,
+    y: 70,
+  },
 });
 
 //Initialize enemy
@@ -70,45 +77,6 @@ const keys = {
   },
 };
 
-function rectangularCollision({ rectangle1, rectangle2 }) {
-  return (
-    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
-      rectangle2.position.x &&
-    rectangle1.attackBox.position.x <=
-      rectangle2.position.x + rectangle2.width &&
-    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
-      rectangle2.position.y &&
-    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-  );
-}
-
-function determineWinner({ player, enemy, timerId }) {
-  clearTimeout(timerId);
-  document.querySelector("#displayText").style.display = "flex";
-  if (player.health === enemy.health) {
-    document.querySelector("#displayText").textContent = "Tie";
-    document.querySelector("#displayText").style.fontSize = "40px";
-  } else if (player.health > enemy.health) {
-    document.querySelector("#displayText").textContent = "Player 1 Wins";
-    document.querySelector("#displayText").style.fontSize = "40px";
-  } else if (enemy.health > player.health) {
-    document.querySelector("#displayText").textContent = "Player 2 Wins";
-    document.querySelector("#displayText").style.fontSize = "40px";
-  }
-}
-
-let timer = 60;
-let timerId;
-function decreaseTimer() {
-  if (timer > 0) {
-    timerId = setTimeout(decreaseTimer, 1000);
-    timer--;
-    document.querySelector("#timer").textContent = timer;
-  }
-  if (timer === 0) {
-    determineWinner({ player, enemy, timerId });
-  }
-}
 decreaseTimer();
 
 //Animation loop
@@ -118,7 +86,7 @@ const animate = () => {
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
   player.update();
-  enemy.update();
+  // enemy.update();
 
   player.velocity.x = 0;
   enemy.velocity.x = 0;
