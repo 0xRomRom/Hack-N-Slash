5985fe8f8cc92d4e8cb13ce212916e9850367346
0xromrom <roman0811@live.nl>
2022-07-04 16:38:55 -0700

    Death animation added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //Select canvas & type & size
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
-
+document.body.style.backgroundColor = "black";
 canvas.width = 1280;
 canvas.height = 720;
 
@@ -16,7 +16,7 @@ const background = new Sprite({
     x: 0,
     y: 0,
   },
-  imageSrc: "./img/background.png",
+  imageSrc: "./img/background2.png",
 });
 
 //Initialize player
@@ -37,8 +37,8 @@ const player = new Fighter({
   framesMax: 8,
   scale: 2.5,
   offset: {
-    x: 115,
-    y: 70,
+    x: 105,
+    y: 65,
   },
   sprites: {
     idle: {
@@ -61,6 +61,22 @@ const player = new Fighter({
       imageSrc: "./img/Sprites/Attack1.png",
       framesMax: 6,
     },
+    takeHit: {
+      imageSrc: "./img/Sprites/Take Hit - white silhouette.png",
+      framesMax: 4,
+    },
+    death: {
+      imageSrc: "./img/Sprites/Death.png",
+      framesMax: 6,
+    },
+  },
+  attackBox: {
+    offset: {
+      x: 120,
+      y: 150,
+    },
+    width: 140,
+    height: 50,
   },
 });
 
@@ -78,6 +94,51 @@ const enemy = new Fighter({
   offset: {
     x: -50,
     y: 0,
+  },
+  imageSrc: "./img/SpritesV2/Idle.png",
+  framesMax: 4,
+  scale: 2.5,
+  offset: {
+    x: 115,
+    y: 80,
+  },
+  sprites: {
+    idle: {
+      imageSrc: "./img/SpritesV2/Idle.png",
+      framesMax: 4,
+    },
+    run: {
+      imageSrc: "./img/SpritesV2/Run.png",
+      framesMax: 8,
+    },
+    jump: {
+      imageSrc: "./img/SpritesV2/Jump.png",
+      framesMax: 2,
+    },
+    fall: {
+      imageSrc: "./img/SpritesV2/Fall.png",
+      framesMax: 2,
+    },
+    attack1: {
+      imageSrc: "./img/SpritesV2/Attack1.png",
+      framesMax: 4,
+    },
+    takeHit: {
+      imageSrc: "./img/SpritesV2/Take hit.png",
+      framesMax: 3,
+    },
+    death: {
+      imageSrc: "./img/SpritesV2/Death.png",
+      framesMax: 7,
+    },
+  },
+  attackBox: {
+    offset: {
+      x: -170,
+      y: 150,
+    },
+    width: 140,
+    height: 50,
   },
 });
 
@@ -108,7 +169,7 @@ const animate = () => {
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
   player.update();
-  // enemy.update();
+  enemy.update();
 
   player.velocity.x = 0;
   enemy.velocity.x = 0;
@@ -133,33 +194,58 @@ const animate = () => {
   if (keys.ArrowLeft.pressed && enemy.lastKey === "ArrowLeft") {
     //Enemy movement
     enemy.velocity.x = -5;
+    enemy.switchSprite("run");
   } else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
+    enemy.switchSprite("run");
     enemy.velocity.x = 5;
+  } else {
+    enemy.switchSprite("idle");
   }
 
-  //Detect for collision
+  //Jumping
+  if (enemy.velocity.y < 0) {
+    enemy.switchSprite("jump");
+  } else if (enemy.velocity.y > 0) {
+    enemy.switchSprite("fall");
+  }
+
+  //Detect for collision & enemy gets hit
   if (
     rectangularCollision({
       rectangle1: player,
       rectangle2: enemy,
     }) &&
-    player.isAttacking
+    player.isAttacking &&
+    player.framesCurrent === 4
   ) {
+    enemy.takeHit();
     player.isAttacking = false;
-    enemy.health -= 20;
     document.querySelector("#enemyHealth").style.width = enemy.health + "%";
   }
 
+  //If player misses
+  if (player.isAttacking && player.framesCurrent === 4) {
+    player.isAttacking = false;
+  }
+
+  //Detect for collision & player gets hit
   if (
     rectangularCollision({
       rectangle1: enemy,
       rectangle2: player,
     }) &&
-    enemy.isAttacking
+    enemy.isAttacking &&
+    enemy.framesCurrent === 2
   ) {
+    player.takeHit();
     enemy.isAttacking = false;
-    player.health -= 20;
+
     document.querySelector("#playerHealth").style.width = player.health + "%";
+  }
+
+  // If Enemy misses
+  if (enemy.isAttacking && enemy.framesCurrent === 2) {
+    enemy.isAttacking = false;
   }
 
   // End game based on health
@@ -171,48 +257,52 @@ const animate = () => {
 animate();
 
 window.addEventListener("keydown", (e) => {
-  console.log(e.key);
-  switch (e.key) {
-    case "d":
-      keys.d.pressed = true;
-      player.lastKey = "d";
-      break;
-    case "D":
-      keys.d.pressed = true;
-      player.lastKey = "d";
-      break;
-    case "a":
-      keys.a.pressed = true;
-      player.lastKey = "a";
-      break;
-    case "A":
-      keys.a.pressed = true;
-      player.lastKey = "a";
-      break;
-    case "w":
-      player.velocity.y = -30;
-      break;
-    case "W":
-      player.velocity.y = -30;
-      break;
-    case " ":
-      player.attack();
-      break;
-
-    case "ArrowRight":
-      keys.ArrowRight.pressed = true;
-      enemy.lastKey = "ArrowRight";
-      break;
-    case "ArrowLeft":
-      keys.ArrowLeft.pressed = true;
-      enemy.lastKey = "ArrowLeft";
-      break;
-    case "ArrowUp":
-      enemy.velocity.y = -20;
-      break;
-    case "ArrowDown":
-      enemy.attack();
-      break;
+  if (!player.dead) {
+    switch (e.key) {
+      case "d":
+        keys.d.pressed = true;
+        player.lastKey = "d";
+        break;
+      case "D":
+        keys.d.pressed = true;
+        player.lastKey = "d";
+        break;
+      case "a":
+        keys.a.pressed = true;
+        player.lastKey = "a";
+        break;
+      case "A":
+        keys.a.pressed = true;
+        player.lastKey = "a";
+        break;
+      case "w":
+        player.velocity.y = -30;
+        break;
+      case "W":
+        player.velocity.y = -30;
+        break;
+      case " ":
+        player.attack();
+        break;
+    }
+  }
+  if (!enemy.dead) {
+    switch (e.key) {
+      case "ArrowRight":
+        keys.ArrowRight.pressed = true;
+        enemy.lastKey = "ArrowRight";
+        break;
+      case "ArrowLeft":
+        keys.ArrowLeft.pressed = true;
+        enemy.lastKey = "ArrowLeft";
+        break;
+      case "ArrowUp":
+        enemy.velocity.y = -20;
+        break;
+      case "ArrowDown":
+        enemy.attack();
+        break;
+    }
   }
 });
 
--- js/classes.js
+++ js/classes.js
@@ -62,6 +62,11 @@ class Fighter extends Sprite {
     framesMax = 1,
     offset = { x: 0, y: 0 },
     sprites,
+    attackBox = {
+      offset: {},
+      width: undefined,
+      height: undefined,
+    },
   }) {
     super({
       position,
@@ -80,9 +85,9 @@ class Fighter extends Sprite {
         x: this.position.x,
         y: this.position.y,
       },
-      offset,
-      width: 100,
-      height: 50,
+      offset: attackBox.offset,
+      width: attackBox.width,
+      height: attackBox.height,
     };
     this.color = color;
     this.isAttacking;
@@ -91,6 +96,7 @@ class Fighter extends Sprite {
     this.framesElapsed = 0;
     this.framesHold = 5;
     this.sprites = sprites;
+    this.dead = false;
 
     for (const sprite in this.sprites) {
       sprites[sprite].image = new Image();
@@ -103,10 +109,19 @@ class Fighter extends Sprite {
   //Player movement
   update() {
     this.draw();
-    this.animateFrames();
+    if (!this.dead) this.animateFrames();
 
+    //Attack Boxes
     this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-    this.attackBox.position.y = this.position.y;
+    this.attackBox.position.y = this.position.y + this.attackBox.offset.y;
+
+    // Draw Attackbox
+    // c.fillRect(
+    //   this.attackBox.position.x,
+    //   this.attackBox.position.y,
+    //   this.attackBox.width,
+    //   this.attackBox.height
+    // );
 
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
@@ -125,17 +140,37 @@ class Fighter extends Sprite {
   attack() {
     this.switchSprite("attack1");
     this.isAttacking = true;
-    setTimeout(() => {
-      this.isAttacking = false;
-    }, 100);
+  }
+
+  takeHit() {
+    this.health -= 20;
+
+    if (this.health <= 0) {
+      this.switchSprite("death");
+    } else this.switchSprite("takeHit");
   }
 
   switchSprite(sprite) {
+    if (this.image === this.sprites.death.image) {
+      if (this.framesCurrent === this.sprites.death.framesMax - 1)
+        this.dead = true;
+      return;
+    }
+
+    //Overriding all others animations with the attack animation
     if (
       this.image === this.sprites.attack1.image &&
       this.framesCurrent < this.sprites.attack1.framesMax - 1
     )
       return;
+
+    //Override when fighter gets hit
+    if (
+      this.image === this.sprites.takeHit.image &&
+      this.framesCurrent < this.sprites.takeHit.framesMax - 1
+    )
+      return;
+
     switch (sprite) {
       case "idle":
         if (this.image !== this.sprites.idle.image) {
@@ -144,6 +179,7 @@ class Fighter extends Sprite {
           this.framesCurrent = 0;
         }
         break;
+
       case "run":
         if (this.image !== this.sprites.run.image) {
           this.image = this.sprites.run.image;
@@ -151,6 +187,7 @@ class Fighter extends Sprite {
           this.framesCurrent = 0;
         }
         break;
+
       case "jump":
         if (this.image !== this.sprites.jump.image) {
           this.image = this.sprites.jump.image;
@@ -158,6 +195,7 @@ class Fighter extends Sprite {
           this.framesCurrent = 0;
         }
         break;
+
       case "fall":
         if (this.image !== this.sprites.fall.image) {
           this.image = this.sprites.fall.image;
@@ -165,10 +203,27 @@ class Fighter extends Sprite {
           this.framesCurrent = 0;
         }
         break;
+
       case "attack1":
         if (this.image !== this.sprites.attack1.image) {
           this.image = this.sprites.attack1.image;
           this.framesMax = this.sprites.attack1.framesMax;
+          this.framesCurrent = 0;
+        }
+        break;
+
+      case "takeHit":
+        if (this.image !== this.sprites.takeHit.image) {
+          this.image = this.sprites.takeHit.image;
+          this.framesMax = this.sprites.takeHit.framesMax;
+          this.framesCurrent = 0;
+        }
+        break;
+
+      case "death":
+        if (this.image !== this.sprites.death.image) {
+          this.image = this.sprites.death.image;
+          this.framesMax = this.sprites.death.framesMax;
           this.framesCurrent = 0;
         }
         break;
